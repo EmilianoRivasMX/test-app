2f4487c9373a6d91fd522e6fff21aca3903e570a
EmilianoRivasMX <44332756+EmilianoRivasMX@users.noreply.github.com>
2024-03-29 05:02:03 -0600

    Add enrollments and courses

--- js/index.js
+++ js/index.js
@@ -1,70 +1,34 @@
-
-// Crea datos aleatorios para la tabla
-let datos = [{
-    "id": 1,
-    "nombre_curso": "Karate",
-    "instructor": "Bruce Lee",
-    "horario": "9:00 - 11:00",
-    "nivel_del_curso": "Intermediate",
-    "fecha_inicio": "Oct 10 2022",
-}, {
-    "id": 2,
-    "nombre_curso": "Karate",
-    "instructor": "Chuck Norris",
-    "horario": "13:00 - 15:00",
-    "nivel_del_curso": "Advanced",
-    "fecha_inicio": "Oct 15 2022",
-}, {
-    "id": 3,
-    "nombre_curso": "Karate",
-    "instructor": "Jet Li",
-    "horario": "16:00 - 18:00",
-    "nivel_del_curso": "Beginner",
-    "fecha_inicio": "Oct 20 2022",
-}, {
-    "id": 4,
-    "nombre_curso": "Karate",
-    "instructor": "Jackie Chan",
-    "horario": "10:00 - 12:00",
-    "nivel_del_curso": "Intermediate",
-    "fecha_inicio": "Oct 25 2022",
-}];
-
-// let datos = []
-
+// Cursos
 function leerCursos() {
-    // Si no hay datos no muestres la tabla
-    if (datos.length === 0) {
+    // Si no hay datos, no muestres la tabla
+    if (sessionStorage.getItem("cursos") === null || JSON.parse(sessionStorage.getItem("cursos")).length === 0 ){
         document.getElementById("tabla").style.display = "none";
         return;
     }
 
-    // Alamcenar la lista de instructores en el sessionStorage
-    sessionStorage.setItem("instructores", JSON.stringify(datos));
-    
-    // Obtener la tabla con id "cursos"
+    // Obtener la tabla con id "cuerpoTabla"
     var tabla = document.getElementById("cuerpoTabla");
 
-    // Obtener la lista de instructores del sessionStorage
-    var instructoresData = JSON.parse(sessionStorage.getItem("instructores"));
+    // Obtener la lista de cursos del sessionStorage
+    var cursos = JSON.parse(sessionStorage.getItem("cursos"));
 
-    filas = ""
+    filas = "";
 
-    // Recorrer la lista de usuarios e instertarlos en la tabla
-    instructoresData.map((instructor, i) => {
+    // Recorrer la lista de cursos e insertarlos en la tabla
+    cursos.forEach((curso, i) => {
         filas += `
             <tr>
-                <td>${instructor.id}</td>
-                <td>${instructor.nombre_curso}</td>
-                <td>${instructor.instructor}</td>
-                <td>${instructor.horario}</td>
-                <td>${instructor.nivel_del_curso}</td>
-                <td>${instructor.fecha_inicio}</td>
+                <td>${curso.id}</td>
+                <td>${curso.nombre_curso}</td>
+                <td>${curso.instructor}</td>
+                <td>${curso.horario}</td>
+                <td>${curso.nivel_del_curso}</td>
+                <td>${curso.fecha_inicio}</td>
                 <td><a class="delete-button" onclick="eliminarCurso(${i})"><i class="fa-solid fa-trash"></i></a></td>
                 <td><a class="edit-button" onclick="editarCurso(${i})"><i class="fa-solid fa-pen-to-square"></i></a></td>
             </tr>
-        `
-    })
+        `;
+    });
 
     // Insertar las filas en la tabla
     tabla.innerHTML = filas;
@@ -78,49 +42,130 @@ function crearCurso() {
     var nivel = document.getElementById("nivel").value;
     var fechaInicio = document.getElementById("fecha_inicio").value;
 
+    // Obtener la lista de cursos del sessionStorage o inicializarla si es null
+    var cursos = JSON.parse(sessionStorage.getItem("cursos")) || [];
+
     // Crear un objeto con los valores del formulario
     var curso = {
-        "id": datos.length + 1,
+        "id": cursos.length + 1,
         "nombre_curso": nombreCurso,
         "instructor": instructor,
         "horario": horario,
         "nivel_del_curso": nivel,
         "fecha_inicio": fechaInicio
-    }
-    
-    // Redirigir a la página principal
-    window.location.href = "cursos.html";
+    };
 
     // Agregar el curso a la lista de cursos
-    datos.push(curso);
+    cursos.push(curso);
+
+    // Almacenar la lista actualizada en el sessionStorage
+    sessionStorage.setItem("cursos", JSON.stringify(cursos));
 
     // Recargar la tabla
     leerCursos();
 }
 
 function editarCurso(id) {
-    // Obtener los valores del curso a editar
-    var curso = datos[id];
-    console.log(curso)
-    // Redirigir a la página de edición
-    window.location.href = "editarCurso.html";
+    // Obtener la lista de cursos del sessionStorage
+    var cursos = JSON.parse(sessionStorage.getItem("cursos"));
 
-    // Insertar los valores del curso en el formulario
-    document.getElementById("id").value
-    document.getElementById("nombre_curso").value = curso.nombre_curso;
-    document.getElementById("instructor").value = curso.instructor;
-    document.getElementById("horario").value = curso.horario;
-    document.getElementById("nivel").value = curso.nivel_del_curso;
-    document.getElementById("fecha_inicio").value = curso.fecha_inicio;
+    // Obtener los valores del curso a editar
+    var curso = cursos[id];
+
+    // Almacenar el curso en sessionStorage para editar posteriormente
+    sessionStorage.setItem("curso_editar", JSON.stringify(curso));
+
+    // Redirigir a la página de edición
+    window.location.href = "nuevoCurso.html";
 }
 
 function eliminarCurso(id) {
     // Pregunta si el usuario está seguro de eliminar el curso
     const confirmacion = confirm("¿Estás seguro de que quieres eliminar este curso?");
-    
-    // Elimina la fila de la tabla y del sessionStorage
+
+    // Elimina el curso de la lista de cursos y actualiza el sessionStorage
     if (confirmacion) {
-        datos.splice(id, 1); 
+        var cursos = JSON.parse(sessionStorage.getItem("cursos"));
+        cursos.splice(id, 1);
+        sessionStorage.setItem("cursos", JSON.stringify(cursos));
         leerCursos();
+    }
+}
+
+
+// Inscripciones
+function leerInscripciones() {
+    if (sessionStorage.getItem("inscripciones") === null || JSON.parse(sessionStorage.getItem("inscripciones")).length === 0) {
+        document.getElementById("tabla").style.display = "none";
+        return;
+    }
+
+    var tabla = document.getElementById("cuerpoTabla");
+
+    var inscripciones = JSON.parse(sessionStorage.getItem("inscripciones"));
+
+    filas = "";
+
+    inscripciones.forEach((inscripcion, i) => {
+        filas += `
+            <tr>
+                <td>${inscripcion.id}</td>
+                <td>${inscripcion.curso}</td>
+                <td>${inscripcion.instructor}</td>
+                <td>${inscripcion.nivel}</td>
+                <td>${inscripcion.horario}</td>
+                <td><a class="delete-button" onclick="eliminarInscripcion(${i})"><i class="fa-solid fa-trash"></i></a></td>
+                <td><a class="edit-button" onclick="editarInscripcion(${i})"><i class="fa-solid fa-pen-to-square"></i></a></td>
+            </tr>
+        `;
+    });
+
+    tabla.innerHTML = filas;
+}
+
+function crearInscripcion() {
+    var curso = document.getElementById("curso").value;
+    var instructor = document.getElementById("instructor").value;
+    var nivel = document.getElementById("nivel").value;
+    var horario = document.getElementById("horario").value;
+
+    var inscripciones = JSON.parse(sessionStorage.getItem("inscripciones")) || [];
+
+    var inscripcion = {
+        "id": inscripciones.length + 1,
+        "curso": curso,
+        "instructor": instructor,
+        "nivel": nivel,
+        "horario": horario,
+    };
+
+    inscripciones.push(inscripcion);
+
+    sessionStorage.setItem("inscripciones", JSON.stringify(inscripciones));
+
+    leerInscripciones();
+}
+
+function editarInscripcion(id) {
+    var inscripciones = JSON.parse(sessionStorage.getItem("inscripciones"));
+    var inscripcion = inscripciones[id];
+    sessionStorage.setItem("inscripcion_editar", JSON.stringify(inscripcion));
+
+    window.location.href = "nuevaInscripcion.html"; 
+
+    // Llenar con los datos de la inscripción a editar
+    // document.getElementById("curso").value = inscripcion.curso;
+    // document.getElementById("instructor").value = inscripcion.instructor;
+    // document.getElementById("nivel").value = inscripcion.nivel;
+    // document.getElementById("horario").value = inscripcion.horario;
+}
+
+function eliminarInscripcion(id) {
+    const confirmacion = confirm("¿Estás seguro de que quieres eliminar esta inscripción?");
+    if (confirmacion) {
+        var inscripciones = JSON.parse(sessionStorage.getItem("inscripciones"));
+        inscripciones.splice(id, 1);
+        sessionStorage.setItem("inscripciones", JSON.stringify(inscripciones));
+        leerInscripciones();
     }
 }
